b7efc8b941a612f1046fd3be6d1078ebfe8d800a
Simonenko Fedor <simonenko.f.a@gmail.com>
2025-10-04 15:35:20 +0300

    feat/docs: add register endpoint

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
     full_name TEXT NOT NULL,
     role TEXT NOT NULL DEFAULT 'user',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
