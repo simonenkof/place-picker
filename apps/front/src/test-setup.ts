5c053d323fedbcf266908df09be71806924df26e
Simonenko Fedor <simonenko.f.a@gmail.com>
2025-11-15 10:18:41 +0300

    test/app: fix tests

--- apps/front/src/test-setup.ts
+++ apps/front/src/test-setup.ts
@@ -1,9 +1,37 @@
 import 'zone.js';
 import 'zone.js/testing';
 
+import { TestBed } from '@angular/core/testing';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { AlertService } from './app/services/alert.service';
 
 setupZoneTestEnv({
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
+});
+
+const alertServiceMock = {
+  show: jest.fn().mockReturnValue(of(void 0)),
+} as unknown as jest.Mocked<AlertService>;
+
+const activatedRouteMock = {
+  snapshot: {
+    paramMap: new Map([['id', '1']]),
+  },
+} as unknown as jest.Mocked<ActivatedRoute>;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    providers: [
+      provideHttpClient(),
+      provideHttpClientTesting(),
+      { provide: AlertService, useValue: alertServiceMock },
+      { provide: ActivatedRoute, useValue: activatedRouteMock },
+    ],
+  });
 });
